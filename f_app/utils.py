de384880ebd013e60d2e098a0b50ba8d214ba553
AhmedIbrahim226 <ae4755740@gmail.com>
2023-07-28 21:13:23 +0300

    finish case

--- f_app/utils.py
+++ f_app/utils.py
@@ -1,32 +1,27 @@
 import json
 
 import requests
-from django.utils import timezone
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 import diff_match_patch as dmp_module
 
 
 def get_url_source_code(url):
     return requests.get(url).text
-def define_0(value):
-    f = [list(x) for x in value]
-    start = None
-    end = None
-    if f[0][0] == 0:
-        start = f[0][1][-150:]
-    if f[-1][0] == 0:
-        end = f[-1][1][:150]
-
-    f[0][1] = start
-    f[-1][1] = end
-    return f
+def custom_0(lis):
+    for idx, _ in enumerate(lis):
+        if idx == 0 and _[0] == 0:
+            yield 0, _[1][-287:]
+        elif idx != 0 and _[0] == 0:
+            yield 0, _[1][:287]
+        else:
+            yield _
 
 
 
 def check_difference(old_source, new_source):
     dmp = dmp_module.diff_match_patch()
     d = dmp.diff_main(old_source, new_source)
-    d = define_0(d)
+    d = list(custom_0(d))
     dmp.diff_cleanupSemantic(d)
     result = dmp.diff_prettyHtml(d)
     return result
